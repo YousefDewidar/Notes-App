6219ad8fcb1f104ea790c8177d4c0975ad5e3bd5
yousefdewidar <ymahmoud1213@gmail.com>
2024-05-23 12:37:31 +0300

    Add circular indicator to bottom sheet

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,26 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_state.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.onTap});
-
+  CustomButton({super.key, this.onTap, this.isLoading = false});
+  bool isLoading;
   final void Function()? onTap;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        width: MediaQuery.of(context).size.width,
-        decoration: const BoxDecoration(
-            color: Colors.deepPurple,
-            borderRadius: BorderRadius.all(Radius.circular(8))),
-        child: const Center(
-            child: Text(
-          'Add',
-          style: TextStyle(color: Colors.white, fontSize: 19),
-        )),
-      ),
-    );
+        onTap: onTap,
+        child: Container(
+            padding: const EdgeInsets.all(10),
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.all(Radius.circular(8))),
+            child: BlocListener<AddNoteCubit, AddNoteState>(
+              listener: (context, state) {
+                if (state is AddNoteLoading) {
+                  isLoading = true;
+                }
+              },
+              child: isLoading == true
+                  ?const Padding(
+                    padding:  EdgeInsets.symmetric(horizontal: 140.0),
+                    child: CircularProgressIndicator(
+                      color: Colors.black,
+                    ),
+                  )
+                  : const Center(
+                      child: Text(
+                        'Add',
+                        style: TextStyle(color: Colors.white, fontSize: 19),
+                      ),
+                    ),
+            )));
   }
 }
