47ac67adc10168b9ac6d95f092b01f50300a87f9
yousefdewidar <ymahmoud1213@gmail.com>
2024-05-23 12:46:29 +0300

    Add circular indicator to bottom sheet

--- lib/widgets/add_bottom_sheet.dart
+++ lib/widgets/add_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_note_cubit.dart';
 import 'package:notes_app/cubits/add_note_state.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
@@ -12,21 +11,20 @@ class AddBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 415,
-        padding: const EdgeInsets.all(24),
-        child: BlocConsumer<AddNoteCubit, AddNoteState>(
-          listener: (context, state) {
-            if (state is AddNoteSuccess) {
-              Navigator.pop(context);
-            }
-            if (state is AddNoteFailuer) {
-              print('failed ${state.errorMsg}');
-            }
-          },
-          builder: (context, state) {
-            return ModalProgressHUD(
-                inAsyncCall: state is AddNoteLoading ? true : false,
-                child: const CustomForm());
-          },
-        ));
+      padding: const EdgeInsets.all(24),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if (state is AddNoteSuccess) {
+            Navigator.pop(context);
+          }
+          if (state is AddNoteFailuer) {
+            print('failed ${state.errorMsg}');
+          }
+        },
+        builder: (context, state) {
+          return const CustomForm();
+        },
+      ),
+    );
   }
 }
