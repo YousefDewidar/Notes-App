15e846efcaf429822c97a032f111eb4d6fe105b3
yousefdewidar <ymahmoud1213@gmail.com>
2024-05-23 12:56:34 +0300

    Fix issues

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_note_cubit.dart';
 import 'package:notes_app/cubits/add_note_state.dart';
 
+// ignore: must_be_immutable
 class CustomButton extends StatelessWidget {
   CustomButton({super.key, this.onTap, this.isLoading = false});
   bool isLoading;
